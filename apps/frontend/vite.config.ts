668e1b3e9e8bca26486bcb0f5f432d5dfb69915c
Emmanuel Isenah <emmanuelisenah@gmail.com>
2024-02-01 17:26:19 +0100

    setup dev mode

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@monorepo/ui": path.resolve(__dirname, "../../packages/ui/src/main.ts"),
+    },
+  },
   plugins: [react()],
-})
+});
